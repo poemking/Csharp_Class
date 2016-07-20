6fc872886763ac70f1e4f995f6d3c779b6055c3f
cheng han,yang <poemofking@gmail.com>
2016-07-21 00:26:42 +0800

    using class get set staic contruct method property
    Signed-off-by: cheng han,yang <poemofking@gmail.com>

---
+++ WinProj_Class/WinProj_Class/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WinProj_Class
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        public class Student 
+        {
+            //add static var
+            private static int count; 
+            private int _no;
+            private string _name;
+            private string _major;
+
+            public void setCount() 
+            {
+                count++;
+            }
+            public int getCount() 
+            {
+                return count;
+            }
+
+            //標準基本建構子
+            public Student() 
+            {
+                _no = -1;
+                _name = "Bob";
+                _major = "Math";
+            }
+
+            //建構子+參數
+            public Student(int i,string n,string m)
+            {
+                _no = i;
+                _name = n;
+                _major = m;
+            }
+            //student method
+            public void Show()
+            {
+                Console.WriteLine("{0} {1} {2}", _no, _name, _major);
+            }
+           //這邊是set get原始概念
+            public int getNo() 
+            {
+                return _no;
+            }
+            public void setNo(int s) 
+            {
+                _no = s;
+            }
+
+            public string getName()
+            {
+                return _name;
+            }
+            public void setName(string s)
+            {
+                _name = s;
+            }
+
+            public string getMajor()
+            {
+                return _major;
+            }
+            public void setMajor(string s)
+            {
+                _major = s;
+            }
+            //到這是set get原始概念
+            public void ShowInfo() 
+            {
+                Console.WriteLine("{0} {1} {2}", _no, _name, _major);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Student s = new Student();
+            s.setNo(1);
+            s.setName("Bruce");
+            s.setMajor("Chinese");
+            s.ShowInfo();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Student s1 = new Student();
+            s1.Show();
+            Student s2 = new Student(2, "Tom", "Math");
+            s2.Show();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Student2 s3 = new Student2();
+            s3.No = 1;
+            s3.Name = "Marry";
+            s3.Major = "English";
+            Console.WriteLine("{0} {1} {2}", s3.No, s3.Name, s3.Major);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //var宣告static時 s1,s2對於count都會存在並改變 s1 count+1 到s2 又+1
+            Student s1 = new Student();
+            s1.setCount();
+ 
+            Student s2 = new Student();
+            Console.WriteLine("{0} {1}", s1.getCount(), s2.getCount());
+            s2.setCount();
+            Console.WriteLine("{0} {1}", s1.getCount(), s2.getCount());
+           
+        }
+    }
+}
---
+++ WinProj_Class/WinProj_Class/Student2.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WinProj_Class
+{
+    class Student2
+    {
+        private int _number;
+        private string _stname;
+        private string _stmajor;
+        //實際用set get 存取器 更快
+        public int No 
+        {
+            set 
+            {
+                _number = value;
+            }
+            get 
+            {
+                return _number;
+            }
+        }
+
+        public string Name
+        {
+            set
+            {
+                _stname = value;
+            }
+            get
+            {
+                return _stname;
+            }
+        }
+
+        public string Major
+        {
+            set
+            {
+                _stmajor = value;
+            }
+            get
+            {
+                return _stmajor;
+            }
+        }
+    }
+}
